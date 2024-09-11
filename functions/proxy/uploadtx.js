a6772c4e9b1524a5f362f07cda0387fead693b11
jokyo1 <148608542+jokyo1@users.noreply.github.com>
2024-09-11 14:43:50 +0800

    Update uploadtx.js

--- functions/proxy/uploadtx.js
+++ functions/proxy/uploadtx.js
@@ -33,7 +33,7 @@ export async function onRequest(context) {
   'Access-Control-Max-Age': '86400', // 24 hours
     };
     // 返回响应
-    return new Response(jsonResponse, {
+    return new Response.json(jsonResponse, {
         status: response.status,
         headers: {
             ...corsHeaders,
