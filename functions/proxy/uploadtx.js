bc6b8d0a7fe65696b5e3845cecf7564bd90022be
jokyo1 <148608542+jokyo1@users.noreply.github.com>
2024-09-11 22:47:14 +0800

    Update uploadtx.js

--- functions/proxy/uploadtx.js
+++ functions/proxy/uploadtx.js
@@ -27,13 +27,15 @@ export async function onRequest(context) {
                 console.log(`${key}:`, value);
             }
     let fileData;
+    // 获取文件
+        fileData = formData.get("file");
+        if (!fileData) {
+            console.log("File not found in formData");
+        }
 
     try {
 
-        // 获取文件
-        fileData = formData.get("file");
-
-        // 创建新的 FormData 对象并添加文件
+            // 创建新的 FormData 对象并添加文件
         const newFormData = new FormData();
         if (fileData) {
             newFormData.append("media", fileData);
