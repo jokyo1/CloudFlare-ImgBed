68f1348c0f2834d92c29c4e3052709802299f4b9
jokyo1 <148608542+jokyo1@users.noreply.github.com>
2024-09-11 17:26:23 +0800

    Update uploadtx.js

--- functions/proxy/uploadtx.js
+++ functions/proxy/uploadtx.js
@@ -25,11 +25,17 @@ export async function onRequest(context) {
 
 // 遍历 FormData 中的所有文件
 //    for (const [key, value] of formData.entries()) {
-    //    if (key === "file" && value instanceof File && isMedia(value)) {
+        if (key === "file" && value instanceof File && isMedia(value)) {
     // 如果字段名是 "file" 且文件是图片或视频，修改字段名为 "media"
-    //    formData.delete(key);
-     //   formData.append('media', value);
-    //  }
+    // 先存储文件数据
+    const fileData = value;
+    
+    // 删除旧的文件字段
+    formData.delete(key);
+    
+    // 添加新的字段
+    formData.append("media", fileData);
+     }
  //   }
       
     // 转发请求
